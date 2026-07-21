9ba917570faae58b0256973328067a75cef6c405
ViksyAsenov <viktorasenov006@gmail.com>
2026-07-22 01:58:40 +0300

    add glm and a simple glsl shader

--- include/config.h
+++ include/config.h
@@ -3,3 +3,10 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <vector>
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,71 @@
 #include <config.h>
 
+unsigned int makeModule(const std::string &filepath, unsigned int moduleType)
+{
+  std::ifstream file;
+  std::stringstream bufferedLines;
+  std::string line;
+
+  file.open(filepath);
+  while (std::getline(file, line))
+  {
+    bufferedLines << line << "\n";
+  }
+
+  std::string shaderSource = bufferedLines.str();
+  const char *shaderSourceCStr = shaderSource.c_str();
+
+  bufferedLines.str("");
+  file.close();
+
+  unsigned int shaderModule = glCreateShader(moduleType);
+  glShaderSource(shaderModule, 1, &shaderSourceCStr, NULL);
+  glCompileShader(shaderModule);
+
+  int success;
+  glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    char errorLog[1024];
+    glGetShaderInfoLog(shaderModule, 1024, NULL, errorLog);
+    std::cout << "Shader module compilation failed: " << errorLog << std::endl;
+  }
+
+  return shaderModule;
+}
+
+unsigned int makeShader(const std::string &vertexFilepath, const std::string &fragmentFilepath)
+{
+  std::vector<unsigned int> shaderModules;
+  shaderModules.push_back(makeModule(vertexFilepath, GL_VERTEX_SHADER));
+  shaderModules.push_back(makeModule(fragmentFilepath, GL_FRAGMENT_SHADER));
+
+  unsigned int shaderProgram = glCreateProgram();
+
+  for (unsigned int shaderModule : shaderModules)
+  {
+    glAttachShader(shaderProgram, shaderModule);
+  }
+
+  glLinkProgram(shaderProgram);
+
+  int success;
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    char errorLog[1024];
+    glGetProgramInfoLog(shaderProgram, 1024, NULL, errorLog);
+    std::cout << "Shader program linking failed: " << errorLog << std::endl;
+  }
+
+  for (unsigned int shaderModule : shaderModules)
+  {
+    glDeleteShader(shaderModule);
+  }
+
+  return shaderProgram;
+}
+
 int main()
 {
   GLFWwindow *window;
@@ -34,7 +100,9 @@ int main()
     return -1;
   }
 
-  glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+  glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+
+  unsigned int shader = makeShader("../src/shaders/vertex.vert", "../src/shaders/fragment.frag");
 
   while (!glfwWindowShouldClose(window))
   {
@@ -42,9 +110,12 @@ int main()
 
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glUseProgram(shader);
+
     glfwSwapBuffers(window);
   }
 
+  glDeleteProgram(shader);
   glfwTerminate();
 
   return 0;
